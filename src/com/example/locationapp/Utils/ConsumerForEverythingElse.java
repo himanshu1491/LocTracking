2948fa4c8fa6b707065ec01b03445303c55f61b1
Himanshu Khandelwal <himanshu1491@gmail.com>
2015-05-17 18:38:24 +0530

    Adding a base consumer class and extending location and everything else from it

--- src/com/example/locationapp/Utils/ConsumerForEverythingElse.java
+++ src/com/example/locationapp/Utils/ConsumerForEverythingElse.java
@@ -4,103 +4,21 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import android.util.Log;
-
-public class ConsumerForEverythingElse extends Thread
+public class ConsumerForEverythingElse<T extends Runnable> extends ConsumerBase<T>
 {
-	private static BlockingQueue<Runnable> queue = null;
+	
 
-	private AtomicBoolean isNetworkAvalable;
-
-	private final String TAG = "ConsumerForEverything";
-
-	public final static ConsumerForEverythingElse _instance = new ConsumerForEverythingElse();
+	public final static ConsumerForEverythingElse<Runnable> _instance = new ConsumerForEverythingElse<Runnable>();
 
 	private ConsumerForEverythingElse()
 	{
-		queue = new LinkedBlockingQueue<Runnable>();
+		queue = (BlockingQueue<T>) new LinkedBlockingQueue<Runnable>();
 		isNetworkAvalable = new AtomicBoolean(Utils.isOnline());
+		TAG="ConsumerForEverything";
 	}
 
-	public static ConsumerForEverythingElse getInstance()
+	public static ConsumerForEverythingElse<Runnable> getInstance()
 	{
 		return _instance;
-	}
-
-	public synchronized void addToQueue(Runnable runnable)
-	{
-		try
-		{
-			Log.d(TAG, "Added to queue" + runnable.hashCode());
-			queue.put(runnable);
-			synchronized (ConsumerForEverythingElse.this)
-			{
-				notify();
-			}
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public synchronized void removeFromQueue(Runnable runnable)
-	{
-		Log.d(TAG,"removing from queue"+runnable.hashCode());
-		queue.remove(runnable);
-	}
-	
-	public void toggleNetworkState()
-	{
-		if (Utils.isOnline())
-		{
-			isNetworkAvalable.set(true);
-
-			synchronized (ConsumerForEverythingElse.this)
-			{
-				notify();
-			}
-		}
-		else
-		{
-			isNetworkAvalable.set(false);
-		}
-	}
-
-	@Override
-	public void run()
-	{
-		while (true)
-		{
-			try
-			{
-				if (queue.isEmpty() || !isNetworkAvalable.get())
-				{
-					Log.d(TAG, "Going in wait condition");
-					synchronized (ConsumerForEverythingElse.this)
-					{
-						wait();
-					}
-					Log.d(TAG, "Waking up");
-					continue;
-				}
-
-				Runnable runnable = queue.peek();
-				if (runnable != null)
-				{
-					runnable.run();
-				}
-			}
-			catch (InterruptedException e)
-			{
-				continue;
-			}
-
-		}
-	}
-
-	public void deleteAll()
-	{
-		queue.clear();
 	}
 }
--- src/com/example/locationapp/Utils/ConsumerLocation.java
+++ src/com/example/locationapp/Utils/ConsumerLocation.java
@@ -4,119 +4,25 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import android.util.Log;
-
 import com.example.locationapp.http.SendLocationToServer;
 
-public class ConsumerLocation extends Thread
+public class ConsumerLocation<T extends Runnable> extends ConsumerBase<T>
 {
 //adb logcat | grep -ni -e  "ConnectionChangeReceiver" -e "ConsumerLocation" -e "LocationDB" 
-	private static BlockingQueue<SendLocationToServer> queue=null;
 	
-	private AtomicBoolean isNetworkAvalable;
-	
-	private final String TAG="ConsumerLocation";
-	
-	public final static  ConsumerLocation _instance=new ConsumerLocation();
+	public final static  ConsumerLocation<SendLocationToServer> _instance=new ConsumerLocation<SendLocationToServer>();
 
 	private  ConsumerLocation()
 	{
-		queue = new LinkedBlockingQueue<SendLocationToServer>();
+		queue = (BlockingQueue<T>) new LinkedBlockingQueue<SendLocationToServer>();
 		isNetworkAvalable=new AtomicBoolean(Utils.isOnline());
+		 TAG="ConsumerLocation";
 	}
 	
-	public static ConsumerLocation getInstance()
+	public static ConsumerLocation<SendLocationToServer> getInstance()
 	{
 		return _instance;
 	}
-
-	public synchronized void addToQueue(SendLocationToServer runnable)
-	{
-		try
-		{
-			Log.d(TAG, "Added to queue"+runnable.timeStamp);
-			queue.put(runnable);
-			synchronized (ConsumerLocation.this)
-			{
-				notify();
-			}
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	public void waitThisThread()
-	{
-		try
-		{
-			wait();
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-	}
 	
-	public synchronized void removeFromQueue(SendLocationToServer runnable)
-	{
-		Log.d(TAG,"removing from queue"+runnable.timeStamp);
-		queue.remove(runnable);
-	}
-	
-	public void toggleNetworkState()
-	{
-		if(Utils.isOnline())
-		{
-			isNetworkAvalable.set(true);
-			
-			synchronized (ConsumerLocation.this)
-			{
-					notify();
-			}			
-		}
-		else
-		{
-			isNetworkAvalable.set(false);
-		}
-	}
-	
-	@Override
-	public void run()
-	{
-		while (true)
-		{
-			try
-			{
-				if(queue.isEmpty()||!isNetworkAvalable.get())
-				{
-					Log.d(TAG, "Going in wait condition");
-					synchronized (ConsumerLocation.this)
-					{
-						wait();
-					}
-					Log.d(TAG,"Waking up");
-					continue;
-				}
-				
-				SendLocationToServer loc = queue.peek();
-				if (loc != null)
-				{
-					loc.run();
-				}
-			}
-			catch (InterruptedException e)
-			{
-				continue;
-			}
-			
-		}
-	}
-
-	public void deleteAll()
-	{
-		queue.clear();
-	}
 	
 }
